49c2443dc8f530f526ce41b1422a5871ed7e84e6
Jacob Gacek <jgacek4@gmail.com>
2019-02-06 21:10:16 -0600

    Added welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,23 @@
 package com.twu.biblioteca;
 
+import java.io.PrintStream;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private PrintStream printStream;
+
+    public BibliotecaApp(PrintStream printStream){
+        this.printStream = printStream;
     }
+
+
+    public void printWelcomeMessage () {
+        printStream.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
+    }
+
+    public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(System.out);
+        bibliotecaApp.printWelcomeMessage();
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,25 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.*;
+
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ExampleTest {
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldWelcomeUserWhenStartingApp() {
+        PrintStream printStream = mock(PrintStream.class);
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(printStream);
+
+        bibliotecaApp.printWelcomeMessage();
+
+        verify(printStream).println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!");
     }
 }
